2997f45ea913e343f13a4b546abeb41468e6047b
DeathsSong <michelle.sherrie@yahoo.com>
2023-09-12 16:31:56 -0700

    Created psuedocode for game elements

--- app.js
+++ app.js
@@ -12,4 +12,38 @@
 ❓Snake Opens Mouth When Near Mouse?
 */
 
-const buttonStart = document.getElementById('button-start')
+const buttonStart = document.getElementById('button-start');
+
+const canvas = document.getElementById('gameCanvas');
+
+const context = canvas.getContext('2d');
+
+const snakeColor = 'green';
+
+const mouseImage = document.getElementsByClassName('mouse')
+
+const gridSize = 20;
+
+
+function createGameBoard() {
+    //Function to set up and display the play area for the game
+    for (let row = 0; row < canvas.height; row += gridSize) {
+
+    }
+}
+
+function createSnake(snakePieces) {
+    //Create the snake
+    //Keep track of snake pieces (aka: when it grows)
+    //Creates each piece as the snake grows
+}
+
+function createMouse(mousePosition) {
+    //Create the mice
+    //Generate the mice in random, unoccupied positions on the board
+}
+
+function gameLoop() {
+    //Make the game loop
+    //Call other functions in a specific order
+}
